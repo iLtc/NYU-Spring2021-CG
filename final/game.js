298aca7c34dc64b5f63da0adcbc2cb33b899e4a5
Alan Luo <i@iltc.io>
2021-05-12 00:03:45 -0400

    Add Split Mode and Panic Mode

--- final/game.js
+++ final/game.js
@@ -1,13 +1,17 @@
 class Ball {
     static scale = 30
 
-    x = 0;
-    y = -23;
+    die = false;
 
-    speed = 0.4;
+    is_hitting_paddle = false;
 
-    directionX = 1;
-    directionY = 1;
+    constructor(x, y, speed, directionX, directionY) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.directionX = directionX;
+        this.directionY = directionY;
+    }
 
     hit_detection(other) {
         if (other instanceof Paddle) {
@@ -17,7 +21,12 @@ class Ball {
                 && this.x / Ball.scale < (other.x + 1) / Paddle.scaleX
                 && Math.abs(this.y / Ball.scale - other.y / Paddle.scaleYZ) < (1 / Ball.scale + 1 / Paddle.scaleYZ)
             ) {
-                this.directionY = -this.directionY
+                if (!this.is_hitting_paddle){
+                    this.directionY = -this.directionY;
+                    this.is_hitting_paddle = true;
+                }
+            } else {
+                this.is_hitting_paddle = false;
             }
         }
 
@@ -30,6 +39,8 @@ class Ball {
             ) {
                 other.destroyed = true;
                 this.directionY = -this.directionY;
+
+                return 'h';
             }
             // Vertical
             else if (
@@ -39,22 +50,33 @@ class Ball {
             ) {
                 other.destroyed = true;
                 this.directionX = -this.directionX;
+
+                return 'v';
             }
         }
     }
 
     next_frame() {
+        if (this.die)
+            return;
+
         this.x += this.speed * this.directionX;
         this.y += this.speed * this.directionY;
 
         if (Math.abs(this.x) > Ball.scale - 1)
             this.directionX = -this.directionX;
 
-        if (Math.abs(this.y) > Ball.scale - 1)
+        if (this.y > Ball.scale - 1)
             this.directionY = -this.directionY;
+
+        if (this.y < - (Ball.scale - 1))
+            this.die = true
     }
 
     draw() {
+        if (this.die)
+            return;
+
         M.save()
         M.scale(1 / Ball.scale)
         M.translate(this.x, this.y, 0)
@@ -109,9 +131,67 @@ class Brick {
 
     destroyed = false;
 
-    constructor(x, y) {
+    constructor(x, y, color, directionX, directionY, speed) {
         this.x = x;
         this.y = y;
+        this.color = color;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.speed = speed;
+    }
+
+    hit_detection(other) {
+        if (other instanceof Paddle) {
+            // Horizontal
+            if (
+                this.x / Brick.scaleX > (other.x - 1) / Paddle.scaleX
+                && this.x / Brick.scaleX < (other.x + 1) / Paddle.scaleX
+                && Math.abs(this.y / Brick.scaleYZ - other.y / Paddle.scaleYZ) < (1 / Brick.scaleYZ + 1 / Paddle.scaleYZ)
+            ) {
+                this.directionY = -this.directionY;
+            }
+            // Vertical
+            else if (
+                this.y / Brick.scaleYZ > (other.y - 1) / Paddle.scaleYZ
+                && this.y / Brick.scaleYZ < (other.y + 1) / Paddle.scaleYZ
+                && Math.abs(this.x / Brick.scaleX - other.x / Paddle.scaleX) < (1 / Brick.scaleX + 1 / Paddle.scaleX)
+            ) {
+                this.directionX = -this.directionX;
+            }
+        }
+
+        if (other instanceof Brick) {
+            // Horizontal
+            if (
+                this.x / Brick.scaleX > (other.x - 1) / Brick.scaleX
+                && this.x / Brick.scaleX < (other.x + 1) / Brick.scaleX
+                && Math.abs(this.y / Brick.scaleYZ - other.y / Brick.scaleYZ) < (1 / Brick.scaleYZ + 1 / Brick.scaleYZ)
+            ) {
+                this.directionY = -this.directionY;
+            }
+            // Vertical
+            else if (
+                this.y / Brick.scaleYZ > (other.y - 1) / Brick.scaleYZ
+                && this.y / Brick.scaleYZ < (other.y + 1) / Brick.scaleYZ
+                && Math.abs(this.x / Brick.scaleX - other.x / Brick.scaleX) < (1 / Brick.scaleX + 1 / Brick.scaleX)
+            ) {
+                this.directionX = -this.directionX;
+            }
+        }
+    }
+
+    next_frame() {
+        if (this.destroyed)
+            return;
+
+        this.x += this.speed * this.directionX;
+        this.y += this.speed * this.directionY;
+
+        if (Math.abs(this.x) > Brick.scaleX - 1)
+            this.directionX = -this.directionX;
+
+        if (Math.abs(this.y) > Brick.scaleYZ - 1)
+            this.directionY = -this.directionY;
     }
 
     draw() {
@@ -121,7 +201,7 @@ class Brick {
         M.save();
         M.scale(1 / Brick.scaleX, 1 / Brick.scaleYZ, 1 / Brick.scaleYZ)
         M.translate(this.x, this.y, 0)
-        drawMesh(cubeMesh, 'green')
+        drawMesh(cubeMesh, this.color)
         M.restore();
     }
 }
